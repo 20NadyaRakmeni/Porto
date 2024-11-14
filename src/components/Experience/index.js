cf939bb64ef8054df2bf55c08d83803f93ac6b22
20NadyaRakmeni <s22129049@student.unklab.ac.id>
2024-11-15 02:53:42 +0800

    feat: firebase into experience

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,9 +1,21 @@
 import React from "react";
-
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
 const Experience = () => {
+  const [experience, setExperience] = useState({});
+
+  useEffect(() => {
+    const db = getDatabase();
+    const experienceRef = ref(db, "experience/");
+
+    onValue(experienceRef, (snapshot) => {
+      const data = snapshot.val();
+      setExperience(data);
+    });
+  }, []);
   return (
     <section className="experience">
-      <h2>Pendidikan</h2>
+      <h2>{experience.title}</h2>
       <ul className="experience-list">
         <li>
           <strong>SMA Advent Waropan 2019-2021</strong>
@@ -13,25 +25,20 @@ const Experience = () => {
         </li>
       </ul>
 
-      <h2>Pengalaman Kerja</h2>
+      <h2>{experience.title1}</h2>
       <ul className="experience-list">
         <li>
-          <strong>Posisi, Perusahaan</strong> - Tahun Mulai - Tahun Selesai
-          <p>Tanggung jawab dan pencapaian di posisi ini.</p>
+          <strong>Posisi, Perusahaan</strong>
         </li>
         <li>
-          <strong>Posisi, Perusahaan</strong> - Tahun Mulai - Tahun Selesai
-          <p>Tanggung jawab dan pencapaian di posisi ini.</p>
+          <strong>Posisi, Perusahaan</strong>
         </li>
       </ul>
 
-      <h2>Keterampilan</h2>
+      <h2>{experience.title2}</h2>
       <ul className="skills-list">
-        <li>Keterampilan 1</li>
-        <li>Keterampilan 2</li>
-        <li>Keterampilan 3</li>
-        <li>Keterampilan 4</li>
-        <li>Keterampilan 5</li>
+        <li>{experience.subTitle}</li>
+        <li>{experience.subTitle2}</li>
       </ul>
     </section>
   );
